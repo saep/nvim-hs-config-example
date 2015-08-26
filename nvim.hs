4251bd2750a88b0c5d0fe267106727c775b6158e
Sebastian Witte <woozletoff@gmail.com>
2015-08-26 10:05:47 +0200

    Remove example plugins

--- nvim.hs
+++ nvim.hs
@@ -1,9 +1,6 @@
 import Neovim
 
-import qualified Fibonacci as Fibonacci
-import qualified Random    as Random
-
 main :: IO ()
 main = neovim defaultConfig
-    { plugins = plugins defaultConfig ++ [ Fibonacci.plugin, Random.plugin ]
+    { plugins = plugins defaultConfig ++ []
     }
